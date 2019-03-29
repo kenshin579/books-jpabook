caf5a569419c271ad28657e8c40a590d497ea390
Frank <frankyoh@tmon.co.kr>
2019-03-29 22:25:31 +0900

    added assert statement

--- ch02-jpa-start1/src/test/java/jpabook/start/MemberTest.java
+++ ch02-jpa-start1/src/test/java/jpabook/start/MemberTest.java
@@ -9,6 +9,8 @@ import org.junit.Test;
 import javax.persistence.EntityManager;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @Slf4j
 public class MemberTest extends JpaTestBase {
 	EntityManager em;
@@ -38,11 +40,13 @@ public class MemberTest extends JpaTestBase {
 			//한건 조회
 			Member findMember = em.find(Member.class, id);
 			log.info("username: {} age: {}", findMember.getUsername(), findMember.getAge());
+			assertThat(findMember.getAge()).isEqualTo(20);
 
 			//목록 조회
 
 			List<Member> members = em.createQuery("FROM Member m", Member.class).getResultList();
 			log.info("members size : {}", members.size());
+			assertThat(members.size()).isEqualTo(1);
 
 			//삭제
 			em.remove(member);
